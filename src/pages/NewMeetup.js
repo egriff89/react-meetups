f7861e31e801af99f216548f7fccf5c37a880e38
Eric Griffith <eric.griffith42@gmail.com>
2021-11-26 00:22:47 -0600

    Integrated with Firebase for storage and retrieval of meetups

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
-import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm />
-    </section>;
+	const history = useHistory();
+
+	function handleAddMeetup(meetupData) {
+		fetch('https://react-meetups-96063-default-rtdb.firebaseio.com/meetups.json', {
+			method: 'POST',
+			body: JSON.stringify(meetupData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then(() => {
+			history.replace('/');
+		});
+	}
+
+	return (
+		<section>
+			<h1>Add New Meetup</h1>
+			<NewMeetupForm onAddMeetup={handleAddMeetup} />
+		</section>
+	);
 }
 
 export default NewMeetupPage;
