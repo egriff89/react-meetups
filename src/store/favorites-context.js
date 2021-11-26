057b7cf30e88ca927a929784570afa6a799f31c6
Eric Griffith <eric.griffith42@gmail.com>
2021-11-26 01:12:06 -0600

    Create context store to track user favorites

---
+++ src/store/favorites-context.js
@@ -0,0 +1,43 @@
+import { createContext, useState } from 'react';
+
+const FavoritesContext = createContext({
+	favorites: [],
+	totalFavorites: 0,
+    addFavorite: (favoriteMeetup) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetupId) => {},
+});
+
+export function FavoritesContextProvider({ children }) {
+    const [userFavorites, setUserFavorites] = useState([]);
+    
+    function handleAddFavorite(favoriteMeetup) {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.concat(favoriteMeetup);
+        });
+    }
+
+    function handleRemoveFavorite(meetupId) {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+        });
+    }
+
+    function handleItemIsFavorite(meetupId) {
+        return userFavorites.some(meetup => meetup.id === meetupId);
+    }
+
+    const context = {
+        favorites: userFavorites,
+        totalFavorites: userFavorites.length,
+        addFavorite: handleAddFavorite,
+        removeFavorite: handleRemoveFavorite,
+        itemIsFavorite: handleItemIsFavorite
+    };
+
+	return <FavoritesContext.Provider value={context}>
+        {children}
+    </FavoritesContext.Provider>;
+}
+
+export default FavoritesContext;
